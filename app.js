7dc2b12621158ceb10a00987bb00a79a9fa9a43c
jd4545 <jamesdeane@msn.com>
2022-03-07 11:01:51 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-const express = require('express');
-const { handleInvalidEndpoint, handlePSQL, handleCustomErrors, handle500 } = require('./error.controllers');
-const {getTopics, getArticleById, patchArticleById, getUsers, getArticles,
-     getComments, postComment, deleteComment, describeEndpoints} = require('./controllers');
+const express = require("express");
+const cors = require("cors");
+const {
+  handleInvalidEndpoint,
+  handlePSQL,
+  handleCustomErrors,
+  handle500,
+} = require("./error.controllers");
+const {
+  getTopics,
+  getArticleById,
+  patchArticleById,
+  getUsers,
+  getArticles,
+  getComments,
+  postComment,
+  deleteComment,
+  describeEndpoints,
+} = require("./controllers");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.get('/api/topics', getTopics);
+app.get("/api/topics", getTopics);
 
-app.get('/api/articles/:article_id', getArticleById);
+app.get("/api/articles/:article_id", getArticleById);
 
-app.patch('/api/articles/:article_id', patchArticleById);
+app.patch("/api/articles/:article_id", patchArticleById);
 
-app.get('/api/users', getUsers);
+app.get("/api/users", getUsers);
 
-app.get('/api/articles', getArticles);
+app.get("/api/articles", getArticles);
 
-app.get('/api/articles/:article_id/comments', getComments)
+app.get("/api/articles/:article_id/comments", getComments);
 
-app.post('/api/articles/:article_id/comments', postComment)
+app.post("/api/articles/:article_id/comments", postComment);
 
-app.delete('/api/comments/:comment_id', deleteComment)
+app.delete("/api/comments/:comment_id", deleteComment);
 
-app.get('/api', describeEndpoints)
+app.get("/api", describeEndpoints);
 
 //========ERRORS:==========
 
